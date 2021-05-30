252d17a744de1fec8a65cc311d38f7a52e805a3a
kris9854 <kris9854@gmial.com>
2021-05-30 19:54:21 +0200

    Started developement of win10functions

--- Modules/Dev/LKKORP-DeploymentShare/LKKORPDeploymentShare/Applications/Win10-LKKORP_Theme 1.0/LKKORP-Win10Functions.ps1
+++ Modules/Dev/LKKORP-DeploymentShare/LKKORPDeploymentShare/Applications/Win10-LKKORP_Theme 1.0/LKKORP-Win10Functions.ps1
@@ -5,19 +5,36 @@ function Change-Win10LockScreen {
         # Main Win10 lockscreenfolder
         [Parameter(Mandatory = $false)]
         [string]
-        $Win10LockscreenDir
+        $Win10LockscreenDir = 'C:\Windows\Web\Screen'
     )
-    
+    #Take ownership
+    takeown /f C:\Windows\Web\Screen\*.*
+
+    #change permission giing system access to write
+    icacls c:\windows\WEB\wallpaper\Windows\*.* /Grant 'System:(F)'
+
+    # Remove originals
+    Remove-Item C:\Windows\Web\Screen\*.*
+
+    #Copy our wallpapers
+    Copy-Item $PSScriptRoot\*.* C:\Windows\Web\Screen
 }
 
 function Change-Wallpaper {
-    param (
-        # Main Win10 lockscreenfolder
-        [Parameter(Mandatory = $false)]
-        [string]
-        $Win10WallpaperDir
-    )
-    
+    param ()
+    #inspiration link https://ccmexec.com/2015/08/replacing-default-wallpaper-in-windows-10-using-scriptmdtsccm/
+    #Take ownership
+    takeown /f c:\windows\WEB\wallpaper\Windows\img0.jpg
+    takeown /f C:\Windows\Web\4K\Wallpaper\Windows\*.*
+    #change permission giing system access to write
+    icacls c:\windows\WEB\wallpaper\Windows\img0.jpg /Grant 'System:(F)'
+    icacls C:\Windows\Web\4K\Wallpaper\Windows\*.* /Grant 'System:(F)'
+    # Remove originals
+    Remove-Item c:\windows\WEB\wallpaper\Windows\img0.jpg
+    Remove-Item C:\Windows\Web\4K\Wallpaper\Windows\*.*
+    #Copy our wallpapers
+    Copy-Item $PSScriptRoot\img0.jpg c:\windows\WEB\wallpaper\Windows\img0.jpg
+    Copy-Item $PSScriptRoot\4k\*.* C:\Windows\Web\4K\Wallpaper\Windows
 
 
 
