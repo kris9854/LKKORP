5e3a7461882443fefcc9eab6ef2d272889d16517
kris9854 <kris9854@gmial.com>
2021-05-30 19:45:47 +0200

    update

---
+++ Modules/Dev/LKKORP-DeploymentShare/LKKORPDeploymentShare/Applications/Win10-LKKORP_Theme 1.0/LKKORP-Win10Functions.ps1
@@ -0,0 +1,24 @@
+# Holds function for the application to work
+
+function Change-Win10LockScreen {
+    param (
+        # Main Win10 lockscreenfolder
+        [Parameter(Mandatory = $false)]
+        [string]
+        $Win10LockscreenDir
+    )
+    
+}
+
+function Change-Wallpaper {
+    param (
+        # Main Win10 lockscreenfolder
+        [Parameter(Mandatory = $false)]
+        [string]
+        $Win10WallpaperDir
+    )
+    
+
+
+
+}
