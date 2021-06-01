cc5df69ce630db3f9ee1c47541595809d2f07853
kris9854 <kris9854@gmial.com>
2021-06-01 13:06:10 +0200

    updaate robo task

--- Modules/Dev/Not_For_Running_with_script/Task Schedules/RoboCopy.ps1
+++ Modules/Dev/Not_For_Running_with_script/Task Schedules/RoboCopy.ps1
@@ -1,11 +1,41 @@
+#Function Call
+Function Write-Log {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $False)]
+        [ValidateSet("INFO", "WARN", "ERROR", "FATAL", "DEBUG")]
+        [String]
+        $Level = "INFO",
+
+        [Parameter(Mandatory = $True)]
+        [string]
+        $Message,
+
+        [Parameter(Mandatory = $False)]
+        [string]
+        $logfile
+    )
+
+    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+    $Line = "$Stamp $Level $Message"
+    If ($logfile) {
+        Add-Content $logfile -Value $Line
+    }
+    Else {
+        Write-Output $Line
+    }
+}
+
 #Robocopy Script
+[int]$startMs = (Get-Date).Millisecond
+#Description used to sync files accross the network
 #variables
-[string]$SourcePath = ''
-[string]$DestinationPath = ''
-[string]$LogFileLocation = ''
+[string]$SourcePath = 'd:\LKKORPDeploymentShare'
+[string]$DestinationPath = '\\DKHER-WDSV0001\LKKORPDeploymentShare$'
+[string]$LogFileLocation = 'C:\Projects\RoboCopy\Log\RoboCopy.log'
 #Call Robocopy with parameters 
 
-Robocopy.exe $SourcePath $DestinationPath /E /ZB /DCOPY:DAT /COPYALL /R:5 /W:10 /LOG:$LogFileLocation
+Robocopy.exe $SourcePath $DestinationPath /E /TEE /ZB /MIR /DCOPY:DAT /COPYALL /R:5 /W:10 /LOG:$LogFileLocation
 
 <#
 Attributes justification:
@@ -17,3 +47,6 @@ Attributes justification:
 /W:10 = Wait 10 sec before restart
 /LOG:$LogFileLocation = logfile location
 #>
+[int]$endMs = (Get-Date).Millisecond
+Write-Host $($startMs - $endMs)
+Write-Log -Level INFO -Message "$($startMs - $endMs)" -logfile "$LogFileLocation"
