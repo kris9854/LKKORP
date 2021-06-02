c6623e1c217a649a799c4ceefd1a70c9809336fc
kris9854 <kris9854@gmial.com>
2021-06-02 21:31:12 +0200

    RoboCopy

--- Modules/Dev/Task Scheduling And wds scheduled scripts/RoboCopy/RoboCopy.ps1
+++ Modules/Dev/Task Scheduling And wds scheduled scripts/RoboCopy/RoboCopy.ps1
@@ -1,45 +1,18 @@
-#Function Call
-Function Write-Log {
-    [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory = $False)]
-        [ValidateSet("INFO", "WARN", "ERROR", "FATAL", "DEBUG")]
-        [String]
-        $Level = "INFO",
-
-        [Parameter(Mandatory = $True)]
-        [string]
-        $Message,
-
-        [Parameter(Mandatory = $False)]
-        [string]
-        $logfile
-    )
-
-    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
-    $Line = "$Stamp $Level $Message"
-    If ($logfile) {
-        Add-Content $logfile -Value $Line
-    }
-    Else {
-        Write-Output $Line
-    }
-}
-
 #Robocopy Script
 [int]$startMs = (Get-Date).Millisecond
 #Description used to sync files accross the network
 #variables
 [string]$SourcePath = 'd:\LKKORPDeploymentShare'
-[string]$DestinationPath = '\\DKHER-WDSV0001\LKKORPDeploymentShare$'
-[string]$LogFileLocation = 'C:\Projects\RoboCopy\Log\RoboCopy.log'
+[string]$DestinationPath = '\\DKVIB-WDSV0001\d$\LKKORPDeploymentShare'
+[string]$LogFileLocation = 'C:\Projects\RoboCopy\RobocopyLog.txt'
 #Call Robocopy with parameters 
 
-Robocopy.exe $SourcePath $DestinationPath /E /TEE /xd DfsrPrivate /ZB /MIR /DCOPY:DAT /COPYALL /R:5 /W:10 /LOG:$LogFileLocation
+Robocopy.exe $SourcePath $DestinationPath /E /TEE /xd DfsrPrivate /ZB /MIR /COPY:DAT /COPYALL /R:5 /W:10 /LOG:$LogFileLocation
 
 <#
 Attributes justification:
 /E = takes all files and subfolders from the source to destination
+/TEE = writes what would be sent to $LogFileLocation in console window
 /ZB = retry to copy the files
 /DCOPY:DAT = insures that attributes, ntfs permissions, timestaps +more
 /COPYALL = Copies all file info, attributes, ntfs permissions, timestaps +more
@@ -48,5 +21,5 @@ Attributes justification:
 /LOG:$LogFileLocation = logfile location
 #>
 [int]$endMs = (Get-Date).Millisecond
-Write-Host $($startMs - $endMs)
-Write-Log -Level INFO -Message "$($startMs - $endMs)" -logfile "$LogFileLocation"
+$Total = "$($startMs - $endMs)"
+Write-Output $Total
