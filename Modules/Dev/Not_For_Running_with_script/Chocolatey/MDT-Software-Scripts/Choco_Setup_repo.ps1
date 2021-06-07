8bb9cd010d08def35444ac0e5a55ae7a40e6851f
kris9854 <kris9854@gmail.com>
2021-06-07 13:15:52 +0200

    Update repo source

--- Modules/Dev/Not_For_Running_with_script/Chocolatey/MDT-Software-Scripts/Choco_Setup_repo.ps1
+++ Modules/Dev/Not_For_Running_with_script/Chocolatey/MDT-Software-Scripts/Choco_Setup_repo.ps1
@@ -1,8 +1,11 @@
 $ChocoRepoURL = 'http://localhost:8081/repository/Choco-Group/'
 
 choco source remove -n chocolatey
-Choco source add -n LKKORP_Proxy -s=http://localhost:8081/repository/Choco-Group/
-Choco source add -n LKKORP_Hosted -s=http://localhost:8081/repository/Choco-Hosted/
+# Remove Standard source repo so we only pull from explicit set repositories
+choco source remove -n chocolatey -y
+
+# Set the source feed/repositorie to LKKORP-Internal
+Choco source add -n 'LKKORP-Internal' -s=http://proget.lkkorp.local/nuget/LKKORP-Internal/ -y
 
 
 choco install chocolateygui
